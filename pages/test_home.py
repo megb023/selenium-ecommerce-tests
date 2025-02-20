3794050999f294301cf0795b33337a5378b75cc5
Matthew Bailes <matt.bailes23@gmail.com>
2025-02-19 17:33:25 -0800

    selenium project setup

---
+++ pages/test_home.py
@@ -0,0 +1,16 @@
+"""
+This module contains page object for the homepage of https://demo.opencart.com/
+"""
+
+class OpenCartHomePage:
+
+    def __init__(self, browser):
+        self.browser = browser
+    
+    def load(self):
+        # TODO
+        pass
+
+    def search(self, phrase):
+        # TODO
+        pass
